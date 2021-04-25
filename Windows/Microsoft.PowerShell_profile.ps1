e279539b78568d689aae87a1146cb2fd1055b669
eddiechaoinchina <eddiechaoinchina@gmail.com>
2021-04-25 15:54:01 +0800

    Modified.

---
+++ Windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+Import-Module posh-git
+Import-Module oh-my-posh
+
+Set-Theme Paradox
+
+Remove-Item alias:diff -force
