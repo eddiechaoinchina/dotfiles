79128ead01736d3a8a7c0b0dda8377f496e00122
eddiechaoinchina <eddiechaoinchina@gmail.com>
2021-04-26 16:56:31 +0800

    Modified.

--- Windows/Microsoft.PowerShell_profile.ps1
+++ Windows/Microsoft.PowerShell_profile.ps1
@@ -7,6 +7,9 @@ if (Test-Path($ChocolateyProfile)) {
 Import-Module posh-git
 Import-Module oh-my-posh
 
-Set-Theme Paradox
+Set-Theme robbyrussell
 
 Remove-Item alias:diff -force
+Remove-Item alias:cat -force
+
+Set-Alias cat bat
