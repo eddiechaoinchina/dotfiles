8e569295f5302b8075a850e430cc05a764468b8a
eddiechaoinchina <eddiechaoinchina@gmail.com>
2021-04-27 08:28:34 +0800

    Modified.

--- Windows/Microsoft.PowerShell_profile.ps1
+++ Windows/Microsoft.PowerShell_profile.ps1
@@ -1,31 +1,27 @@
 # Author: Will Chao <nerdzzh@gmail.com>
 # Filename: Microsoft.PowerShell_profile.ps1
-# Last Change: 2021/4/27 0:17:29 +0800
+# Last Change: 2021/4/27 8:27:58 +0800
 # Brief: My PowerShell_profile File
 
 # Preamble -------------------------------------- {{{
 
-# Install chocolatey first.
-
-# Install "bat" with choco.
-
-# Insatll "curl" and "es".
-
+# Install chocolatey first and install "bat" with it, this will install "less"
+# alongside. The chocolatey profile was generated automatically during the
+# installation, remove it before use.
+#
 # Install "coreutils", "grep", "diffutils", "less", "tree" from gnuwin32
-# project.
-
+# project. Change the name "tree" to "treeview" to avoid confilcts with
+# powershell's original tree command.
+#
+# Insatll "curl" and "es", both great tools.
+#
 # Follow steps on microsoft webpage to configure the theme.
 
 # }}}
 
+# Variables ------------------------------------- {{{
+
 $env:BAT_THEME="Coldark-Dark"
-
-# Initialize theme ------------------------------ {{{
-
-Import-Module posh-git
-Import-Module oh-my-posh
-
-Set-Theme robbyrussell
 
 # }}}
 
@@ -34,25 +30,29 @@ Set-Theme robbyrussell
 Remove-Item alias:cp -force
 Remove-Item alias:ls -force
 Remove-Item alias:rm -force
-
 Remove-Item alias:cat -force
 Remove-Item alias:dir -force
 Remove-Item alias:pwd -force
-
 Remove-Item alias:diff -force
 Remove-Item alias:echo -force
 Remove-Item alias:curl -force
-
 Remove-Item alias:rmdir -force
 
-Set-Alias tree "tree /f"
 Set-Alias cat bat
+Set-Alias tree treeview
+
+# }}}
+
+# Theme ----------------------------------------- {{{
+
+Import-Module posh-git
+Import-Module oh-my-posh
+
+Set-Theme robbyrussell
 
 # }}}
 
 # Chocolatey profile ---------------------------- {{{
-
-# This is added automatically by choco installation.
 
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
