9fe108a43c85cfed1582672b3d2f86ebce97e1f3
eddiechaoinchina <eddiechaoinchina@gmail.com>
2021-04-28 09:54:49 +0800

    Modified.

---
+++ init.fish
@@ -0,0 +1,37 @@
+# Author: Will Chao <nerdzzh@gmail.com>
+# Filename: init.fish
+# Last Change: 04/28/21 09:49:52 +0800
+# Brief: My init.fish File
+
+# Preamble -------------------------------------- {{{
+
+# Fish - The Friendly Interactive Shell
+#
+# The reason why I switched from bash and zsh to fish is mainly because of its
+# easy scripting and good portability, as well as its fantastic oh-my-fish
+# framework for plugin and theme management. Though I have to learn fish
+# scripting in addition to bash, it is quite easy to pick up.
+#
+# The oh-my-fish framework has provided plenty of useful plugins and themes with
+# easy installation. The theme I am currently using is the classic robbyrussell,
+# which is simple, clean and at the same time pretty clear.
+#
+# Here are the fish plugins I cannot live without:
+# --------------------------------------------
+# fzf          | fuzzy finder          | <c-t>
+# z            | jump between folders  |
+# --------------------------------------------
+#
+# The fzf plugin needs prerequisite of the fzf package.
+
+# }}}
+
+# Variables ------------------------------------- {{{
+
+# Path --------------------- {{{
+
+set --export PATH $PATH /home/zzh/anaconda3/bin /home/zzh/.local/bin
+
+# }}}
+
+# }}}
