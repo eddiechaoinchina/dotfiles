099ed9c63343e4ea83658ab4f3ca1de6eb7bebfc
eddiechaoinchina <eddiechaoinchina@gmail.com>
2021-05-13 16:00:24 +0800

    Modified.

--- mklink.sh
+++ mklink.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 # Author: Will Chao <nerdzzh@gmail.com>
 # Filename: mklink.sh
-# Last Change: 05/11/21 17:43:11 +0800
+# Last Change: 05/13/21 15:21:10 +0800
 # Brief: Make symlink for dotfiles.
 
 DOTFILES_PATH=$(dirname $(realpath -s $0))
 LINK_CMD="ln -s --force --verbose"
 
+$LINK_CMD $DOTFILES_PATH/.gitconfig ~/.gitconfig
 $LINK_CMD $DOTFILES_PATH/.tmux.conf ~/.tmux.conf
-$LINK_CMD $DOTFILES_PATH/init.fish $OMF_CONFIG/init.fish
 $LINK_CMD $DOTFILES_PATH/.vimrc ~/.vimrc
+$LINK_CMD $DOTFILES_PATH/init.fish $OMF_CONFIG/init.fish
